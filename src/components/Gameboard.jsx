252429793f234b13cb685592cc979cbacf5858bb
goddi1999 <godfredasefa@icloud.com>
2025-09-08 14:15:41 +0200

    add

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,15 +7,32 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [...prevGameBoard.map((row) => [...row])];
+      updatedGameBoard[rowIndex][colIndex] = "X";
+
+      return updatedGameBoard;
+    });
+  };
   return (
     <div className="grid grid-cols-3 gap-2">
-      {initialGameBoard.flat().map((playerSymbol, index) => (
-        <button
-          key={index}
-          className="border border-gray-300 flex items-center justify-center h-16 w-16"
-        >
-          {playerSymbol}
-        </button>
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex} className="list-none">
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <button
+                key={`${rowIndex}-${colIndex}`}
+                onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                className="border border-gray-300 flex items-center justify-center h-16 w-16"
+              >
+                {playerSymbol}
+              </button>
+            ))}
+          </ol>
+        </li>
       ))}
     </div>
   );
