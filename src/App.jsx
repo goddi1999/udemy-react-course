91e7423603b0c4e1dcebcd6d76d41204a0cca378
goddi1999 <godfredasefa@icloud.com>
2025-09-13 19:22:28 +0200

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import Player from "./components/Player";
-import GameBoard from "./components/Gameboard";
 export default function App() {
   return (
     <main className="bg-gray-100 min-h-screen flex justify-center items-center font-sans">
-      <div className="w-[1020px] rounded-2xl mx-auto p-6 border bg-black text-white flex flex-col items-center">
-        <ol className="w-full grid grid-cols-2 gap-4 text-center text-2xl font-bold mb-6">
-          <Player initialName="Player 2" symbol="O" />
-          <Player initialName="Player 1" symbol="X" />
-        </ol>
-        <GameBoard />
-      </div>
+      <h1 className="text-3xl font-bold underline"></h1>
+      Hello world!
     </main>
   );
 }
